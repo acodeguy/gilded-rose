851c1223cee4865533e12b004c45b2cabc558569
Andrew <24258342+acodeguy@users.noreply.github.com>
2019-04-13 09:52:12 +0100

    mock tests for #update_quality to make sure it is called for every item given

--- spec/gilded_rose_spec.rb
+++ spec/gilded_rose_spec.rb
@@ -2,4 +2,11 @@ require 'gilded_rose'
 
 describe GildedRose do
 
+  it '#update_quality is called on each item given' do
+    sword = double('sword')
+    gun = double('gun')
+    expect(sword).to receive_messages(alter_quality: nil)
+    expect(gun).to receive_messages(alter_quality: nil)
+    GildedRose.new([sword, gun]).update_quality
+  end
 end
