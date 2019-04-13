86c10df56333376bac2003d0851110992b95957a
Andrew <24258342+acodeguy@users.noreply.github.com>
2019-04-13 09:17:28 +0100

    move legendary item tests to own file as actual and not mocked tests

--- spec/gilded_rose_spec.rb
+++ spec/gilded_rose_spec.rb
@@ -1,26 +1,5 @@
 require 'gilded_rose'
-require 'item'
-require 'normal_item'
-require 'legendary_item'
 
 describe GildedRose do
 
-  describe "#update_quality" do
-
-    context 'when item is Sulfuras, Hand of Ragnaros' do
-      it 'does not change sell_in' do
-        legendary_item = double('Aged Brie')
-        allow(legendary_item).to receive_messages(name: 'Sulfuras, Hand of Ragnaros', sell_in: 1, alter_quality: 0)
-        GildedRose.new([legendary_item]).update_quality
-        expect(legendary_item.sell_in).to eq(1)
-      end
-
-      it 'does not change quality' do
-        legendary_item = double('Aged Brie')
-        allow(legendary_item).to receive_messages(name: 'Sulfuras, Hand of Ragnaros', quality: 80, alter_quality: 0)
-        GildedRose.new([legendary_item]).update_quality
-        expect(legendary_item.quality).to eq(80)
-      end
-    end
-  end
 end
