fa2870df9c3c74f3f5bb6f55cd5e3c01b8f1c254
Andrew <24258342+acodeguy@users.noreply.github.com>
2019-04-13 09:14:10 +0100

    correct mocked tests for actual ones

--- spec/normal_item_spec.rb
+++ spec/normal_item_spec.rb
@@ -71,66 +71,58 @@ describe Item do
   context 'when item is Backstage passes to a TAFKAL80ETC concert' do
     context 'when sell_in is greater than 10' do
       it 'increases quality by one after each day' do
-        normal_item = double('Backstage passes to a TAFKAL80ETC concert')
-        allow(normal_item).to receive_messages(name: 'Backstage passes to a TAFKAL80ETC concert', quality: 1, alter_quality: 0)
-        GildedRose.new([normal_item]).update_quality
-        expect(normal_item.quality).to eq(1)
+        item = NormalItem.new("Backstage passes to a TAFKAL80ETC concert", 15, 0)
+        GildedRose.new([item]).update_quality
+        expect(item.quality).to eq(1)
       end
 
       it 'never increases quality beyond 50' do
-        normal_item = double('Backstage passes to a TAFKAL80ETC concert')
-        allow(normal_item).to receive_messages(name: 'Backstage passes to a TAFKAL80ETC concert', quality: 50, alter_quality: 0)
-        GildedRose.new([normal_item]).update_quality
-        expect(normal_item.quality).to eq(50)
+        item = NormalItem.new("Backstage passes to a TAFKAL80ETC concert", 15, 50)
+        GildedRose.new([item]).update_quality
+        expect(item.quality).to eq(50)
       end
     end
 
     context 'when sell_in is less than or equal to 10 but more than 5' do
       it 'increases quality by two after each day' do
-        normal_item = double('Backstage passes to a TAFKAL80ETC concert')
-        allow(normal_item).to receive_messages(name: 'Backstage passes to a TAFKAL80ETC concert', quality: 2, alter_quality: 0)
-        GildedRose.new([normal_item]).update_quality
-        expect(normal_item.quality).to eq(2)
+        item = NormalItem.new("Backstage passes to a TAFKAL80ETC concert", 10, 0)
+        GildedRose.new([item]).update_quality
+        expect(item.quality).to eq(2)
       end
 
       it 'only raises quality to 50 when quality is at 49' do
-        normal_item = double('Backstage passes to a TAFKAL80ETC concert')
-        allow(normal_item).to receive_messages(name: 'Backstage passes to a TAFKAL80ETC concert', quality: 50, alter_quality: 0)
-        GildedRose.new([normal_item]).update_quality
-        expect(normal_item.quality).to eq(50)
+        item = NormalItem.new("Backstage passes to a TAFKAL80ETC concert", 10, 49)
+        GildedRose.new([item]).update_quality
+        expect(item.quality).to eq(50)
       end
     end
 
     context 'when sell_in is less than or equal to 5 but more than 0' do
       it 'increases quality by three after each day' do
-        normal_item = double('Backstage passes to a TAFKAL80ETC concert')
-        allow(normal_item).to receive_messages(name: 'Backstage passes to a TAFKAL80ETC concert', quality: 3, alter_quality: 0)
-        GildedRose.new([normal_item]).update_quality
-        expect(normal_item.quality).to eq(3)
+        item = NormalItem.new("Backstage passes to a TAFKAL80ETC concert", 5, 0)
+        GildedRose.new([item]).update_quality
+        expect(item.quality).to eq(3)
       end
 
       it 'only raises quality to 50 when quality is at 48 or more' do
-        normal_item = double('Backstage passes to a TAFKAL80ETC concert')
-        allow(normal_item).to receive_messages(name: 'Backstage passes to a TAFKAL80ETC concert', quality: 50, alter_quality: 0)
-        GildedRose.new([normal_item]).update_quality
-        expect(normal_item.quality).to eq(50)
+        item = NormalItem.new("Backstage passes to a TAFKAL80ETC concert", 5, 48)
+        GildedRose.new([item]).update_quality
+        expect(item.quality).to eq(50)
       end
     end
 
     context 'after sell_in' do
       it 'sets quality to 0' do
-        normal_item = double('Backstage passes to a TAFKAL80ETC concert')
-        allow(normal_item).to receive_messages(name: 'Backstage passes to a TAFKAL80ETC concert', quality: 0, alter_quality: 0)
-        GildedRose.new([normal_item]).update_quality
-        expect(normal_item.quality).to eq(0)
+        item = NormalItem.new("Backstage passes to a TAFKAL80ETC concert", 0, 50)
+        GildedRose.new([item]).update_quality
+        expect(item.quality).to eq(0)
       end
 
       it 'does not change quality' do
-        normal_item = double('Backstage passes to a TAFKAL80ETC concert')
-        allow(normal_item).to receive_messages(name: 'Backstage passes to a TAFKAL80ETC concert', quality: 0, alter_quality: 0)
-        GildedRose.new([normal_item]).update_quality
-        expect(normal_item.quality).to eq(0)
+        item = NormalItem.new("Backstage passes to a TAFKAL80ETC concert", 0, 0)
+        GildedRose.new([item]).update_quality
+        expect(item.quality).to eq(0)
       end
-    end
+    end  
   end
 end
